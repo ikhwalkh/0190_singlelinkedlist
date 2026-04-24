ffa0f4b74d20eac6b84c20b426be6ae32904f924
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-24 15:16:28 +0700

    first commit

---
+++ singly_linkedlist.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int noMhs;
+    Node *next;
+};
+
+class Linkedlist
+{
+    Node *START;
+
+    public:
+    Linkedlist()
+    {
+        START
+    }
+}
