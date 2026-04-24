fc19507388650ce291caabccf6c6620b3786bf1a
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-24 15:34:37 +0700

    while

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -39,6 +39,20 @@ class Linkedlist
 
             Node *previous = START;
             Node *current = START;
+
+            while ((current != NULL) && (nim >= current->noMhs))
+            {
+                if (nim == current->noMhs)
+                {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return;
+                }
+                previous = current;
+                current = current->next;
+            }
+
+            nodeBaru->next = current;
+            previous->next = nodeBaru;
         }
     }
 }
