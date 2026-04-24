49fc530d845800dcc4d45eb36c6a98c40d804b31
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-04-24 16:38:22 +0700

    case

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -170,8 +170,36 @@ int main()
                     cout << "\nList Kosong\n";
                     break;
                 }
-            }
+                Node *previous, *current;
+                      cout << endl
+                      << "Masukkan no mahasiswa yang dicari : ";
+                      cin >> nim;
+
+                     if (mhs.search(nim, &previous, &current) == false)
+                         cout << endl
+                         << "Data tidak ditemukan" << endl;
+                        else
+                         {
+                           cout << endl
+                           << "Data ditemukan" << endl;
+                           cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                           cout << "\n";
+                         }
+
+                    }
+                    break;
+                    case '5':
+                    {
+                    }
+                    break;
+                    default:
+                    {
+                        cout << "pilihan salah !." << endl;
+                    }
+                    break;
+                }
+            } while (ch != '5');
         }
-    }
+        
     
-}
+    
